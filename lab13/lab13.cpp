19bc96167c3a0c9534b7c1b47072588ae5a14a73
Carri Martin <cmartin028@student.butte.edu>
2017-09-01 02:05:26 +0000

    new submission lab13

--- lab13/lab13.cpp
+++ lab13/lab13.cpp
@@ -17,7 +17,7 @@ int main () {
     int deaths_per_year = ((60 * 60 * 24 * 365) / 12); //This is 1 death every 12 seconds//
     int migrants_per_year = ((60 * 60 * 24 * 365) / 33); //This is 1 international migrant every 33 seconds//
     int net_gain_per_year = birth_per_year + migrants_per_year - deaths_per_year;
-    int num_years_past_2017 = 99;
+    int num_years_past_2017 = 8;
     int future_population = us_population_2017 + net_gain_per_year * num_years_past_2017;
     int this_year = 2017;
     int future_year = this_year + num_years_past_2017;
@@ -31,3 +31,15 @@ int main () {
     
     
     }
+    
+//This year, 2017, the approximate population in the United States is 325773883.
+//Every year we have an approximate net gain of 2269636 people in our country.
+//Using the numbers available, in 99 years, 2116, the population will be approximatly 550467847 people living in the US.//
+
+//This year, 2017, the approximate population in the United States is 325773883.
+//Every year we have an approximate net gain of 2269636 people in our country.
+//Using the numbers available, in 26 years, 2043, the population will be approximatly 384784419 people living in the US.//
+
+//This year, 2017, the approximate population in the United States is 325773883.
+//Every year we have an approximate net gain of 2269636 people in our country.
+//Using the numbers available, in 8 years, 2025, the population will be approximatly 343930971 people living in the US.//
