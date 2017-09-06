88802e1af3766d1b3700c77c54fd148df11cd7e7
Carri Martin <cmartin028@student.butte.edu>
2017-09-06 03:02:47 +0000

    Lab 1.4 submission

--- lab14/lab14.cpp
+++ lab14/lab14.cpp
@@ -28,9 +28,9 @@ int main () {
        numPennies  = centsIn%5;
     
     cout << "If you put " << centsIn << " into the CoinStar machine, that would be " << numQuarters << endl;
-    cout << " quarters, " << numDimes << " dimes, " << numNickels << " nickels and " << numPennies << " pennies." << endl;
+    cout << "quarters, " << numDimes << " dimes, " << numNickels << " nickels and " << numPennies << " pennies." << endl;
     
-    cout << " Enter the amouht of money, in dollars: "; //Input using a decimal//
+    cout << "Enter the amouht of money, in dollars: "; //Input using a decimal//
     cin >> dollarsIn;
     
        x           = dollarsIn * .109;
@@ -48,24 +48,24 @@ int main () {
 /*Running /home/ubuntu/workspace/lab14/lab14.cpp
 Enter the amount of money, in cents, put into the machine: 105
 If you put 105 into the CoinStar machine, that would be 4
- quarters, 0 dimes, 1 nickels and 0 pennies.
- Enter the amouht of money, in dollars: 1.05
+quarters, 0 dimes, 1 nickels and 0 pennies.
+Enter the amouht of money, in dollars: 1.05
 Now when you deduct the transaction fee of 10.9% from the $1.05 that you placed 
 into the machine, and your total cash in hand will be $0.93555
 
 Running /home/ubuntu/workspace/lab14/lab14.cpp
 Enter the amount of money, in cents, put into the machine: 3459
 If you put 3459 into the CoinStar machine, that would be 138
- quarters, 0 dimes, 1 nickels and 4 pennies.
- Enter the amouht of money, in dollars: 34.59
+quarters, 0 dimes, 1 nickels and 4 pennies.
+Enter the amouht of money, in dollars: 34.59
 Now when you deduct the transaction fee of 10.9% from the $34.59 that you placed 
 into the machine, and your total cash in hand will be $30.8197
 
 Running /home/ubuntu/workspace/lab14/lab14.cpp
 Enter the amount of money, in cents, put into the machine: 987654321
 If you put 987654321 into the CoinStar machine, that would be 39506172
- quarters, 2 dimes, 0 nickels and 1 pennies.
- Enter the amouht of money, in dollars: 9876543.21
+quarters, 2 dimes, 0 nickels and 1 pennies.
+Enter the amouht of money, in dollars: 9876543.21
 Now when you deduct the transaction fee of 10.9% from the $9.87654e+06 that you placed 
 into the machine, and your total cash in hand will be $8.8e+06
 
