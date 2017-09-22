509c5b3399a286f4992b05ca11a9e8a50d6237ac
Carri Martin <cmartin028@student.butte.edu>
2017-09-22 06:46:16 +0000

    Lab2.2 submission

--- lab22/lab22.cpp
+++ lab22/lab22.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
 #include <cstdlib> //C standard library for random numbers
+#include <ctime>
 using namespace std;
 
 int main () {
+    srand (time(0)); //Unique seed
     int random_number1 = 0;
     int random_number2 = 0;
     
-    random_number1 = 3;
-    random_number2 = 11;
-    srand(random_number1);
-    srand(random_number2); 
     
    random_number1 = ((rand() % 100) + 1); //This will pick a random number between 1 and 100
    random_number2 = ((rand() % 100) + 1); //This will pick a differnet random number between 1 and 100
-   cout << "A pseudo-random number between 1 and 100 " << random_number1 << endl; //While random, now the code will pick the same number, so,
-   cout << "Another pseudo-random number between 1 and 100 " << random_number2 << endl; //it's really pseudo-random
-   
+   cout << "A random number between 1 and 100 " << random_number1 << endl; //Output statement
+   cout << "Another random number between 1 and 100 " << random_number2 << endl;  //Output statement
    
    return 0;
 }
 /*Running /home/ubuntu/workspace/lab22/lab22.cpp
-A pseudo-random number between 1 and 100 24
-Another pseudo-random number between 1 and 100 95
+A random number between 1 and 100 31
+Another random number between 1 and 100 32
+
+Running /home/ubuntu/workspace/lab22/lab22.cpp
+A random number between 1 and 100 32
+Another random number between 1 and 100 59
+
+Running /home/ubuntu/workspace/lab22/lab22.cpp
+A random number between 1 and 100 32
+Another random number between 1 and 100 59
+
+
 */
