61fca23e24726b9c6b87019f43eba7f22b3de65f
Carri Martin <cmartin028@student.butte.edu>
2017-09-24 18:48:51 +0000

    Assign1 submission

--- assign1/assign1.cpp
+++ assign1/assign1.cpp
@@ -55,6 +55,24 @@ struct employeeWages {
       payStub.mileageReimbur = milesTraveled * mileageRate;
       payStub.totalPay = payStub.netPay + payStub.mileageReimbur;
       
+      cout << "Enter employee first name: ";
+      cin >> firstName;
+      cout << "Enter employee last name: ";
+      cin >> lastName;
+      cout << "Enter employee health insurance cost to be deducted pre-tax: ";
+      cin >> payStub.healthIns;
+      cout << "Enter employee hourly wage: ";
+      cin >> payStub.hourlyWage;
+      cout << "Enter employee hours worked for the week: ";
+      cin >> payStub.workHoursPerWeek;
+      cout << "Enter employee starting odometer reading: ";
+      cin >> startingOdometer;
+      cout << "Enter employee ending odometer reading: ";
+      cin >> endingOdometer;
+      cout << endl;
+      cout << endl;
+      
+      
       cout << firstName << " " << lastName << "    Hourly wage -    " << payStub.hourlyWage << " per hour" << endl;
       cout << endl;
       cout << endl;
@@ -70,5 +88,110 @@ struct employeeWages {
       
       }
       
-    
+    /*Running /home/ubuntu/workspace/assign1/assign1.cpp
+Enter employee first name: Sofia
+Enter employee last name: Cameron
+Enter employee health insurance cost to be deducted pre-tax: 178.65
+Enter employee hourly wage: 23.57
+Enter employee hours worked for the week: 40
+Enter employee starting odometer reading: 3268
+Enter employee ending odometer reading: 4154
+
+
+Sofia Cameron    Hourly wage -    23.57 per hour
+
+
+Gross Pay                 942.8
+
+Net Pay                   634.245
+
+Mileage Reimbursement     310.1
+
+
+Total Pay                 944.344
+
+
+Process exited with code: 0
+
+Running /home/ubuntu/workspace/assign1/assign1.cpp
+Enter employee first name: Richard
+Enter employee last name: Watson
+Enter employee health insurance cost to be deducted pre-tax: 344.57
+Enter employee hourly wage: 11.50
+Enter employee hours worked for the week: 29
+Enter employee starting odometer reading: 9763
+Enter employee ending odometer reading: 10701
+
+
+Richard Watson    Hourly wage -    11.5 per hour
+
+
+Gross Pay                 942.8
+
+Net Pay                   634.245
+
+Mileage Reimbursement     310.1
+
+
+Total Pay                 944.344
+
+
+Process exited with code: 0
+
+Running /home/ubuntu/workspace/assign1/assign1.cpp
+Enter employee first name: Heather
+Enter employee last name: Hunt
+Enter employee health insurance cost to be deducted pre-tax: 224.75
+Enter employee hourly wage: 60.00
+Enter employee hours worked for the week: 35
+Enter employee starting odometer reading: 5705
+Enter employee ending odometer reading: 5705
+
+
+Heather Hunt    Hourly wage -    60 per hour
+
+
+Gross Pay                 942.8
+
+Net Pay                   634.245
+
+Mileage Reimbursement     310.1
+
+
+Total Pay                 944.344
+
+
+Process exited with code: 0
+
+
+
+Running /home/ubuntu/workspace/assign1/assign1.cpp
+Enter employee first name: Eddy
+Enter employee last name: Hall
+Enter employee health insurance cost to be deducted pre-tax: 180.27
+Enter employee hourly wage: 8.25
+Enter employee hours worked for the week: 40
+Enter employee starting odometer reading: 12919
+Enter employee ending odometer reading: 13146
+
+
+Eddy Hall    Hourly wage -    8.25 per hour
+
+
+Gross Pay                 942.8
+
+Net Pay                   634.245
+
+Mileage Reimbursement     310.1
+
+
+Total Pay                 944.344
+
+
+Process exited with code: 0
+
+
+
+
+*/
       
