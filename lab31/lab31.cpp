5886794a2d7dfbe92838dec7887941d75b7e0288
Carri Martin <cmartin028@student.butte.edu>
2017-10-17 06:58:21 +0000

    Lab3.2 submission

--- lab31/lab31.cpp
+++ lab31/lab31.cpp
@@ -9,100 +9,35 @@ This program will determine the best cell phone plan based on the usage
 */
 
 class Plans {
-    
     public:
     
-    void SetAttUnlimitedPlus(int plan_A);
-    void SetAttUnlimitedChoice(int plan_B);
+    void SetAttUnlmtdPlus(int plan_A);
+    void SetAttUnlmtdChoice(int plan_B);
     void SetAttFamily(int plan_C);
-    void SetVerizonUnlimited(int plan_D);
-    void SetSprintUnlimited(int plan_E);
+    void SetVerizonUnlmtd(int plan_D);
+    void SetSprintUnlmtd(int plan_E);
     void SetSprint2GB(int plan_F);
+    void SetPlanFeatures(int f1, int f2, int f3, int f4, f5);
     
-    int GetAttUlimitedPlus() const;
-    int GetAttUnlimitedChoice() const;
+    int GetAttUnlmtdPlus() const;
+    int GetAttUnlmtdChoice() const;
     int GetAttFamily() const;
-    int GetVerizonUnlimited() const;
-    int GetSprintUnlimited() const;
+    int GetVerizonUnlmtd() const;
+    int GetSprintUnlmtd() const;
     int GetSprint2GB() const;
-    
-    void Print();
+    int GetPlanFeatures() const;
     
     private:
     
-    int first_line_price;
-    int price_for_two_lines;
-    int each_additional_phone;
-    int per_tablet;
-    int per_phone;
-    int price_1GB;
-    int price_3GB;
-    int price_6GB;
-    int price_10GB;
-    int price_16GB;
-    int price_25GB;
-    
-    void Plans::SetAttUnlimitedPlus(int plan_A) {
-       return;
-    }
-    
-    void Plans::SetAttUnlimitedChoice(int plan_B) {
-        return;
-    }
-    
-    void Plans::SetAttFamily(int plan_C) {
-        return;
-    }
-    
-    void Plans::SetVerizonUnlimited(int plan_D){
-        return;
-    }
-    
-    void Plans::SetSprintUnlimited(int plan_E) {
-        return;
-    }
-    
-    void Plans::SetStateSalesTax(int plan_F) {
-        return;
-    }
-    
-    int Plans::GetAttUnlimitedChoice() const {
-        return plan_A;
-    }
-    
-    int Plans::GetAttUnlimitedChoice() const {
-        return plan_B;
-    }
-    
-    int Plans::GetAttFamily() const {
-        return plan_C;
-    }
-    
-    int Plans::GetVerizonUnlimited() const {
-        return plan_D;
-    }
-    
-    int Plans::GetSprintUnlimited() const {
-        return plan_E;
-    }
-    
-    int Plans::GetSprint2GB() const {
-        return plan_F;
-    }
-    
-    int main(){
-        string user1;
-        string user2;
-        string user3;
-        
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    int one_line;
+    int two_lines;
+    int add_lines;
+    int add_tablets;
+    int num_gb;
+    string personal_hotspot;
+};
+
+void Plans::SetAttUnlmtdPlus(int plan_A){
+   plan_a = plan_A;
+   return plan_a;
+}
---
+++ lab32/lab32.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+/*Written by Carri Martin
+10-16-2017
+Lab3.2 Switch Case
+This program will compile the gross income, tax paid and taxes either to be refunded, or owed, which is partially determined
+by their marital status.
+*/
+
+double income_tax_table (double tax_married, double tax_singel) {
+    
+    const double personal_exemption = 4050.00;
+    const double single_deduction = 6350.00;
+    const double married_deduction = 12700.00;
+    double gross_wages_single = 0.0;
+    double gross_wages_married = 0.0;
+    double total_tax_paid = 0.0;
+    double total_tax_owed = 0.0;
+    double total_tax_refund = 0.0;
+    
+    if 
+        if (gross_wages_single <= 9325.00) {
+        tax_table = (gross_wages_single - 4050.00) * 0.10;
+}
+        else if (gross_wages_single <= 37950.00) {
+        tax_table = ((gross_wages_single - 9325.00) * 0.15)) + 932.50;
+}
+        else if (gross_wages_single <=91900.00) {
+        tax_table = ((gross_wages_single - 91900.00) * 0.25) + 5226.25;
+}       
+        else if (gross_wages_single <= 416700.00) {
+        tax_table = ((gross_wages_single -191650.00) * 0.33) + 46643.75;
+}
+        else if (gross_wages_single > 416700.00) {
+        tax_table = ((gross_wages_single - 416700.00) * 0.396) + 120910.25;
+}
+}
+
+    else if
+    
+    
+     
+}
+    
+   
+    
+}
+
+int main() {
+    
+    string marital_status(string single, string married);
+    string first_name;
+    string last_name;
+    
+    
+    
+    
+    cout<<"Enter first name:";
+    cin>>first_name;
+    cout<<"Enter last name:";
+    cin>>last_name;
+    cout<<"Enter gross income:";
+    cin>>gross_wages;
+    
+    
+    
+    
+
+    
+    
+    
+    
+
+    
+}
+
