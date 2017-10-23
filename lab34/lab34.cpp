42c152852a6dd623dff932712a8c4fa6a9a6189f
Carri Martin <cmartin028@student.butte.edu>
2017-10-23 05:35:47 +0000

    Lab34 submission

--- lab34/lab34.cpp
+++ lab34/lab34.cpp
@@ -1,39 +1,38 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
 
 /*This program was corrected by Carri Martin
 on 10-21-2017
 This program reads in a letter and reports whether
  it is an uppercase letter, a lowercase letter, or neither.
- it should continue reading in values until the user enters a 0
+ it should continue reading in values until the user enters a -1
 */
  
 int main() {  //Main function
         
-        signed char ch; // Variable in main
-        cout << "Please enter a character, or enter 0 to quit: "; //Intended output
-        cin  >> ch; //user input
-         
-         
-    
-        while (ch != '0') //This will execute a repeating loop as long as the user doesn't input a 0 
-        {
+        
+        char ch; // Variable in main
+        cout << "Please enter a character: "; //Intended output
+        cin>> ch; //user input
+        
+          while (ch != 0) //This will execute a repeating loop as long as the user doesn't input a 0 
+          {
           if ((ch >= 'A') && (ch <= 'Z')) { // check -- is it an uppercase letter??
           
-               cout << "Yes, that is a uppercase letter, enter another character, or to quit, use 0: ";
-               cin>>ch;
+               cout << "Yes, that is a uppercase letter: ";
+               cin>> ch;
           }
     
           else if ((ch >= 'a') && (ch <= 'z')) { //Check if it's a lower case letter
           
-               cout << "Yes, that is a lowercase letter, enter another character, or to quit, use 0: ";
-               cin>>ch;
+               cout << "Yes, that is a lowercase letter: ";
+               cin>> ch;
           }
-         
+          
           else  { //All non-letter charater input
              
-            cout << "Not a letter, enter another character, or to quit, use 0: ";
-            cin>>ch;
+            cout << "Not a letter ";
+            cin>> ch;
         }     
         }
         
@@ -41,10 +40,9 @@ int main() {  //Main function
     }
 
     /*Running /home/ubuntu/workspace/lab34/lab34.cpp
-Please enter a character, or enter 0 to quit: F
-Yes, that is a uppercase letter, enter another character, or to quit, use 0: f
-Yes, that is a lowercase letter, enter another character, or to quit, use 0: Z
-Yes, that is a uppercase letter, enter another character, or to quit, use 0: 3
-Not a letter, enter another character, or to quit, use 0: 0
+Please enter a character: F
+Yes, that is a uppercase letter: f
+Yes, that is a lowercase letter: 3
+Not a letter
 */
       
