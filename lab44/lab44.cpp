47c4f07ace95674163f17cdd3f26e1c19fa36392
Carri Martin <cmartin028@student.butte.edu>
2017-11-15 07:12:42 +0000

    Lab44 submission

--- lab44/lab44.cpp
+++ lab44/lab44.cpp
@@ -7,59 +7,56 @@ Date: November 9, 2017
 Lab 4.4 C Strings
 */
 
-//This program will allow the user to create their own user name for my new website
-
-
+//This program will allow the user to create their own user name for my new website using c strings
 main() {
     
-const int first_name = 10;
+const int first_name = 10; //Constant compiler
 const int last_name = 20;
-char user_first_name[10];
-char user_last_name[20];
-int i = 0;
+char user_first_name[10]; //Array of ten characters max
+char user_last_name[20]; //Array fo twenty characters max
+int i = 0; //Loop index
 
-cout<<"Enter your first name and then your last name: "<<endl;
+//Promt for user's name
+cout<<"Enter your first name and then your last name: "<<endl; 
 cin>>user_first_name>>user_last_name;
 cout<<endl;
 
-
-//while ((i <= strlen(user_first_name) && (i <= strlen(user_last_name)))) {
+    //If statements to print name and give warnings of problems with the user's name  
        if ((strlen(user_first_name) < first_name) && (strlen(user_last_name) < last_name)) {
          if (user_first_name[i] != user_last_name[i]) {
      --i;
 }
-
-             else if (user_first_name[i] == user_last_name[i]) { 
-           cout<<"Warning, first and last name are the same."<<endl;
+         else if (user_first_name[i] == user_last_name[i]) { 
+    cout<<"Warning, first and last name are the same."<<endl;
            //cin>>user_first_name>>user_last_name;
 }
      
 } 
-    else { ((strlen(user_first_name) > first_name) || (strlen(user_last_name) > last_name));
-  cout<<"Your first name or your last name is larger than maximum size allowed."<<endl;
+         else { ((strlen(user_first_name) > first_name) || (strlen(user_last_name) > last_name));
+    cout<<"Your first name or your last name is larger than maximum size allowed."<<endl;
  
-}
-   cout<<"Name: "<<user_first_name<<" "<<user_last_name<<endl;
-   cout<<"Which of the three user names would you like: "<<endl;
-   cout<<endl;
-   
+}   //Print the user's given name and then ask which three options for their username they would like
+    cout<<"Name: "<<user_first_name<<" "<<user_last_name<<endl;
+    cout<<"Which of the three user names would you like: "<<endl;
+    cout<<endl;
+   //For expressions to display the three username options with their name  
    for (i = 0; user_first_name[i] != '\0', user_last_name[i] != '\0'; ++i) {
        user_first_name[i] = tolower(user_first_name[i]);
        user_last_name[i] = tolower(user_last_name[i]);
 }
-   cout<<user_first_name[0]<<user_first_name[1]<<user_last_name<<endl;
+    cout<<user_first_name[0]<<user_first_name[1]<<user_last_name<<endl; //Prints username option 1
    
    for (i = 0; user_first_name[i] != '\0', user_last_name[i] != '\0'; ++i) {
        user_first_name[i] = tolower(user_first_name[i]);
        user_last_name[i] = toupper(user_last_name[i]);
 }
-    cout<<user_first_name<<user_last_name[0]<<endl;
+    cout<<user_first_name<<user_last_name[0]<<endl; //Prints username option 2
     
     for (i = 0; user_first_name[i] != '\0', user_last_name[i] != '\0'; ++i) {
        user_first_name[i] = toupper(user_first_name[i]);
        user_last_name[i] = toupper(user_last_name[i]);
 }
-    cout<<user_first_name[0]<<user_last_name<<endl;
+    cout<<user_first_name[0]<<user_last_name<<endl; //Prints username option 3
    
 
 return 0;
@@ -98,6 +95,21 @@ Which of the three user names would you like:
 anhazeltine-smith
 anaelizabethH
 AHAZELTINE-SMITH
+
+Running /home/ubuntu/workspace/lab44/lab44.cpp
+Enter your first name and then your last name: 
+James James
+
+Warning, first and last name are the same.
+Name: James James
+Which of the three user names would you like: 
+
+jajames
+jamesJ
+JJAMES
+
+
+Process exited with code: 0
 
 
 Process exited with code: 0
