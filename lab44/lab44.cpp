101811c70827360020377296e6fedb17c3c03f1e
Carri Martin <cmartin028@student.butte.edu>
2017-11-15 06:46:32 +0000

    Lab44 submission

--- lab44/lab44.cpp
+++ lab44/lab44.cpp
@@ -20,32 +20,92 @@ int i = 0;
 
 cout<<"Enter your first name and then your last name: "<<endl;
 cin>>user_first_name>>user_last_name;
+cout<<endl;
 
 
 //while ((i <= strlen(user_first_name) && (i <= strlen(user_last_name)))) {
        if ((strlen(user_first_name) < first_name) && (strlen(user_last_name) < last_name)) {
          if (user_first_name[i] != user_last_name[i]) {
-     cout<<"Name: "<<user_first_name<<" "<<user_last_name<<endl;
      --i;
 }
 
              else if (user_first_name[i] == user_last_name[i]) { 
            cout<<"Warning, first and last name are the same."<<endl;
            //cin>>user_first_name>>user_last_name;
-             }
+}
      
 } 
     else { ((strlen(user_first_name) > first_name) || (strlen(user_last_name) > last_name));
   cout<<"Your first name or your last name is larger than maximum size allowed."<<endl;
  
 }
+   cout<<"Name: "<<user_first_name<<" "<<user_last_name<<endl;
    cout<<"Which of the three user names would you like: "<<endl;
-   for (i = 0; user_first_name[i] != '\0'; ++i) {
+   cout<<endl;
+   
+   for (i = 0; user_first_name[i] != '\0', user_last_name[i] != '\0'; ++i) {
        user_first_name[i] = tolower(user_first_name[i]);
-       //user_last_name[i] = tolower(user_last_name[20]);
-   }
+       user_last_name[i] = tolower(user_last_name[i]);
+}
    cout<<user_first_name[0]<<user_first_name[1]<<user_last_name<<endl;
- 
+   
+   for (i = 0; user_first_name[i] != '\0', user_last_name[i] != '\0'; ++i) {
+       user_first_name[i] = tolower(user_first_name[i]);
+       user_last_name[i] = toupper(user_last_name[i]);
+}
+    cout<<user_first_name<<user_last_name[0]<<endl;
+    
+    for (i = 0; user_first_name[i] != '\0', user_last_name[i] != '\0'; ++i) {
+       user_first_name[i] = toupper(user_first_name[i]);
+       user_last_name[i] = toupper(user_last_name[i]);
+}
+    cout<<user_first_name[0]<<user_last_name<<endl;
+   
 
 return 0;
 }
+
+/*Running /home/ubuntu/workspace/lab44/lab44.cpp
+Enter your first name and then your last name: 
+Carri Martin
+
+Name: Carri Martin
+Which of the three user names would you like: 
+
+camartin
+carriM
+CMARTIN
+
+Running /home/ubuntu/workspace/lab44/lab44.cpp
+Enter your first name and then your last name: 
+April Browne
+
+Name: April Browne
+Which of the three user names would you like: 
+
+apbrowne
+aprilB
+ABROWNE
+
+Running /home/ubuntu/workspace/lab44/lab44.cpp
+Enter your first name and then your last name: 
+AnaElizabeth Hazeltine-Smith
+
+Your first name or your last name is larger than maximum size allowed.
+Name: AnaElizabeth Hazeltine-Smith
+Which of the three user names would you like: 
+
+anhazeltine-smith
+anaelizabethH
+AHAZELTINE-SMITH
+
+
+Process exited with code: 0
+
+
+Process exited with code: 0
+
+
+Process exited with code: 0
+
+*/
